e3b34503cbbbc1dae1fc667c254fe099a9d2f53d
Ivan Kosach <kosaci74@gmail.com>
2026-01-08 15:58:48 +0300

    fix

--- core/checker.py
+++ core/checker.py
@@ -15,19 +15,18 @@ async def checker():
                     if user_bot['status'] == 'stop':
                         continue
 
-                    try:
-                        answer = await ping_bot(user_bot['username'])
-                    except Exception as e:
-                        logging.error(f'PING bot error: {e}', exc_info=True)
-                        await tg_client.log_out()
-                        await asyncio.sleep(5)
-                        await tg_client.start()
-                        answer = {'status': 'error'}
-
+                    answer = await ping_bot(user_bot['username'])
                     if answer['status'] != 'ok':
                         await bot.send_message(chat_id=user_id,
                                                text=f'❗️ Бот <a href="https://t.me/{user_bot["username"]}">'
                                                     f'{user_bot['name']}</a> перестал отвечать на команду /start')
+                    else:
+                        logging.error(f'Error bot ping status: {answer}')
+                        if answer.get('error', '') == 'some error':
+                            await tg_client.log_out()
+                            await asyncio.sleep(5)
+                            await tg_client.start()
+
                     await asyncio.sleep(1 + random.random() * 2)
                 except Exception as e:
                     logging.error(e, exc_info=True)
